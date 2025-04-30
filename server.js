ea4e0b8c4036f99ffbb68664c93843b10a4cc6ed
Thales Oliveira <TOliveiraDev@outlook.com>
2025-04-29 21:05:46 -0300

    re-preset api project added base route and livros route

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-import http from "http";
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/": "Curso de Node.js Alura",
-    "/livros": "Livros",
-    "/autores": "Autores"
-};
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor iniciado na porta 3000");
 });
 

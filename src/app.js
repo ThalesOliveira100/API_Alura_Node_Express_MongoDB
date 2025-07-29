748f839fb31daa03ea65d56e2dbc0d6734073bd5
Thales Oliveira <toliveiradev@outlook.com>
2025-07-29 17:00:11 -0300

    feature: conexão com banco de dados mongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js"
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+    console.error("Erro durante a conexão com o banco de dados", erro);
+});
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco de dados estabilizada com sucesso!")
+});
 
 const app = express();
 // Middeware

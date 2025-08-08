56e22811bd3eee6170a884bf270a20f8d60199f3
Thales Oliveira <73021124+ThalesOliveira100@users.noreply.github.com>
2025-08-08 14:29:20 -0300

    :sparkles: feat: Adicionado Route, Controller e Model para Autor

---
+++ src/controllers/autorController.js
@@ -0,0 +1,63 @@
+import autor from "../models/Autor.js";
+
+class AutorController {
+
+    static async listarAutores(req, res) {
+        try {
+            const listaAutores = await autor.find({});
+            res.status(200).json(listaAutor);
+            
+        } catch (error) {
+            res.status(500).json({ message: `Falha na listagem de autores: ${error.message}`});
+        }
+    };
+
+    static async listaAutorPorId (req, res) {
+        try {
+            const autorEncontrado = await autor.findById(req.params.id);
+            res.status(200).json(autorEncontrado);
+
+        } catch (error) {
+            res.status(500).json({message: `Falha na listagem de autor: ${error.message}`});
+        }
+    };
+
+    static async cadastrarAutor(req, res) {
+        try {
+            const novoAutor = await autor.create(req.body);
+
+            res.status(201).json(
+                {
+                    message: `Criado com sucesso`,
+                    autor: novoAutor
+                }
+            );
+            
+        } catch (error) {
+            res.status(500).json({message: `Falha na criação de Autor: ${error.message}`});
+        }
+    };
+
+    static async atualizarAutor (req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Autor atualizado"});
+
+        } catch (error) {
+            res.status(500).json({message: `Falha na atualização do autor: ${error.message}`});
+        }
+    };
+
+    static async excluirAutor (req, res) {
+        try {
+            await autor.findByIdAndDelete(req.params.id);
+            res.status(200).json({ message: "Autor removido com sucesso."})
+
+        } catch (error) {;
+            res.status(500).json({message: `Falha na exclusão do autor: ${error.message}`});
+        }
+    };
+};
+
+export default AutorController;
---
+++ src/routes/autorRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import AutorController from "../controllers/autorController.js";
+
+const routes = express.Router();
+
+routes.get("/v1/autores", AutorController.listarAutores);
+routes.get("/v1/autores/:id", AutorController.listaAutorPorId);
+routes.post("/v1/autores", AutorController.cadastrarAutor);
+routes.put("/v1/autores/:id", AutorController.atualizarAutor);
+routes.delete("/v1/autores:id", AutorController.excluirAutor);
+
+export default routes;

c0e2e67536bb8b09575f60fb4c7e7c42f2177c2e
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-07 22:31:02 -0300

    :sparkles: feat: Adicionado função cadastrarLivros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -7,11 +7,26 @@ class LivroController {
             const listaLivros = await livro.find({});
             res.status(200).json(listaLivros);
             
-        } catch (erro) {
-            res.status(500).json({ message: `Falha na requisição: ${erro.message}`})
+        } catch (error) {
+            res.status(500).json({ message: `Falha na requisição: ${error.message}`})
         }
     }
 
+    static async cadastrarLivro(req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+
+            res.status(201).json(
+                {
+                    message: `Criado com sucesso`,
+                    livro: novoLivro
+                }
+            );
+            
+        } catch (error) {
+            res.status(500).json({message: `Falha na requisição: ${error.message}`})
+        }
+    }
 };
 
 export default LivroController

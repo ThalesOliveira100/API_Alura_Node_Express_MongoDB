e17f71674d30f625bccfac8dedbc7bbafb337325
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-07 22:57:22 -0300

    :sparkles: feat: Adicionado novas rotas de livros para consultar, cadastrar e atualizar livros

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,8 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/v1/livros", LivroController.listarLivros);
+routes.get("/v1/livros/:id", LivroController.listaLivroPorId);
+routes.post("/v1/livros", LivroController.cadastrarLivro);
+routes.put("/v1/livros/:id", LivroController.atualizarLivro);
 
 export default routes;

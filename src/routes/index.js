f50be8c5775f6cf2bf86507db28665e74d659ce5
Thales Oliveira <73021124+ThalesOliveira100@users.noreply.github.com>
2025-08-08 17:30:14 -0300

    :sparkles: feat: Adicionado rota autores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 // Padrão barrel
 import express from "express";
 import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
     app.route("/v1").get((req, res) => res.status(200).send("Curso de API Node Express + MongoDB - Alura"));
     
     // Middewares
-    app.use(express.json(), livros);
+    app.use(express.json(), livros, autores);
 };
 
 export default routes;

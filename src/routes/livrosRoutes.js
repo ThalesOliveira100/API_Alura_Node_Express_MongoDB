7dee80d22486cd795b533119fe6b1d694337d44b
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-07 23:40:30 -0300

    :sparkles: feat: Adicionado rota DELETE para livros

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -7,5 +7,6 @@ routes.get("/v1/livros", LivroController.listarLivros);
 routes.get("/v1/livros/:id", LivroController.listaLivroPorId);
 routes.post("/v1/livros", LivroController.cadastrarLivro);
 routes.put("/v1/livros/:id", LivroController.atualizarLivro);
+routes.delete("/v1/livros/:id", LivroController.excluirLivro);
 
 export default routes;

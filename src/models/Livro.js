0dc83fa481cf33ff0d675a4ea3bfb1b191721f2c
Thales Oliveira <73021124+ThalesOliveira100@users.noreply.github.com>
2025-08-08 17:49:51 -0300

    :sparkles: feat: Adicionado autorSchema no cadastro de livros

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livrosSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
@@ -6,6 +7,7 @@ const livrosSchema = new mongoose.Schema({
     editora: { type: String },
     preco: { type: Number },
     paginas: { type: Number },
+    autor: autorSchema
 }, { versionKey: false });          // versionKey: false    ->     desativa o versionamento do MongoDB
 
 const livro = mongoose.model("livros", livrosSchema);

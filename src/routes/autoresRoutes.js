70b4a3fa425860836509dac59e0c7cdf0d393668
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-09 12:24:03 -0300

    :bug: fix: Corrigido rota de exclusão de autores

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -7,6 +7,6 @@ routes.get("/v1/autores", AutorController.listarAutores);
 routes.get("/v1/autores/:id", AutorController.listaAutorPorId);
 routes.post("/v1/autores", AutorController.cadastrarAutor);
 routes.put("/v1/autores/:id", AutorController.atualizarAutor);
-routes.delete("/v1/autores:id", AutorController.excluirAutor);
+routes.delete("/v1/autores/:id", AutorController.excluirAutor);
 
 export default routes;

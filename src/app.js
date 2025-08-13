64027f553cc0fed40df088e242da456800d65408
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-13 00:02:15 -0300

    :bug: fix: Ajustado exibição de erro durante conexão com o banco

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js";
 
-db.on("error",  console.error("Erro durante a conexão com o banco de dados", erro));
+db.on("error",  console.log.bind("Erro durante a conexão com o banco de dados"));
 db.once("open", () => {
     console.log("Conexão com o banco de dados estabilizada com sucesso!");
 });

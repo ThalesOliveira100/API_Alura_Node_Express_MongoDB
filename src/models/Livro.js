3f311edd291638e57d7a5ae4bc7051ea4b059ce5
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-12 23:47:29 -0300

    :recycle: refactor: Refatorando para seguir com os ajustes indicados no curso.

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
-import { autorSchema } from "./Autor.js";
 
-const livrosSchema = new mongoose.Schema({
-    id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: true },
-    editora: { type: String },
-    preco: { type: Number },
-    paginas: { type: Number },
-    autor: autorSchema
-}, { versionKey: false });          // versionKey: false    ->     desativa o versionamento do MongoDB
+const livrosSchema = new mongoose.Schema(
+    {
+        id: { type: mongoose.Schema.Types.ObjectId },
+        titulo: { type: String, required: true },
+        autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
+        editora: { type: String },
+        preco: { type: Number },
+        paginas: { type: Number }
+    }, 
+    { 
+        versionKey: false 
+    }
+);          // versionKey: false    ->     desativa o versionamento do MongoDB
 
-const livro = mongoose.model("livros", livrosSchema);
+const livros = mongoose.model("livros", livrosSchema);
 
-export default livro;
+export default livros;

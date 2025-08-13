feeba6808735b95b7cb0d145ec1de5e2c2cae81d
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-12 23:47:41 -0300

    :recycle: refactor: Refatorando para seguir com os ajustes indicados no curso.

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,11 +3,12 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
-routes.get("/v1/livros", LivroController.listarLivros);
-routes.get("/v1/livros/busca", LivroController.listarLivrosPorEditora);
-routes.get("/v1/livros/:id", LivroController.listaLivroPorId);
-routes.post("/v1/livros", LivroController.cadastrarLivro);
-routes.put("/v1/livros/:id", LivroController.atualizarLivro);
-routes.delete("/v1/livros/:id", LivroController.excluirLivro);
+routes
+    .get("/v1/livros", LivroController.listarLivros)
+    .get("/v1/livros/busca", LivroController.listarLivrosPorEditora)
+    .get("/v1/livros/:id", LivroController.listaLivroPorId)
+    .post("/v1/livros", LivroController.cadastrarLivro)
+    .put("/v1/livros/:id", LivroController.atualizarLivro)
+    .delete("/v1/livros/:id", LivroController.excluirLivro)
 
 export default routes;

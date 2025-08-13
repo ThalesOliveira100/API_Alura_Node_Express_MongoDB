a021164220422f1d608323ac8fe82eb429fa5171
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-12 23:47:32 -0300

    :recycle: refactor: Refatorando para seguir com os ajustes indicados no curso.

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -3,10 +3,11 @@ import AutorController from "../controllers/autorController.js";
 
 const routes = express.Router();
 
-routes.get("/v1/autores", AutorController.listarAutores);
-routes.get("/v1/autores/:id", AutorController.listaAutorPorId);
-routes.post("/v1/autores", AutorController.cadastrarAutor);
-routes.put("/v1/autores/:id", AutorController.atualizarAutor);
-routes.delete("/v1/autores/:id", AutorController.excluirAutor);
+routes
+    .get("/v1/autores", AutorController.listarAutores)
+    .get("/v1/autores/:id", AutorController.listaAutorPorId)
+    .post("/v1/autores", AutorController.cadastrarAutor)
+    .put("/v1/autores/:id", AutorController.atualizarAutor)
+    .delete("/v1/autores/:id", AutorController.excluirAutor)
 
 export default routes;

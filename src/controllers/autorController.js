443ffb801cdab7c76efa644e93b5107e6bb1f91f
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-12 23:47:16 -0300

    :recycle: refactor: Refatorando para seguir com os ajustes indicados no curso.

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,61 +1,62 @@
-import { autor } from "../models/Autor.js";
+import { autores } from "../models/Autor.js";
 
 class AutorController {
 
     static async listarAutores(req, res) {
         try {
-            const listaAutores = await autor.find({});
+            const listaAutores = await autores.find({});
+
             res.status(200).json(listaAutores);
             
         } catch (error) {
-            res.status(500).json({ message: `Falha na listagem de autores: ${error.message}`});
+            res.status(500).json({ message: `Falha na listagem de autores: ${error.message} `});
         }
     };
 
     static async listaAutorPorId (req, res) {
         try {
-            const autorEncontrado = await autor.findById(req.params.id);
+            const autorEncontrado = await autores.findById(req.params.id);
+
             res.status(200).json(autorEncontrado);
 
         } catch (error) {
-            res.status(500).json({message: `Falha na listagem de autor: ${error.message}`});
+            res.status(500).json({ message: `Falha na listagem de autor: ${error.message}` });
         }
     };
 
     static async cadastrarAutor(req, res) {
         try {
-            const novoAutor = await autor.create(req.body);
+            const autor = new autores(req.body);
 
-            res.status(201).json(
-                {
-                    message: `Criado com sucesso`,
-                    autor: novoAutor
-                }
-            );
+            const autorResultado = await autor.save()
+
+            res.status(201).send(autorResultado.toJSON());
             
         } catch (error) {
-            res.status(500).json({message: `Falha na criação de Autor: ${error.message}`});
+            res.status(500).json({ message: `Falha na criação de Autor: ${error.message}` });
         }
     };
 
     static async atualizarAutor (req, res) {
         try {
             const id = req.params.id;
-            await autor.findByIdAndUpdate(id, req.body);
-            res.status(200).json({ message: "Autor atualizado"});
+
+            await autores.findByIdAndUpdate(id, { $set: req.body });
+
+            res.status(200).json({ message: "Autor atualizado" });
 
         } catch (error) {
-            res.status(500).json({message: `Falha na atualização do autor: ${error.message}`});
+            res.status(500).json({ message: `Falha na atualização do autor: ${error.message}` });
         }
     };
 
     static async excluirAutor (req, res) {
         try {
-            await autor.findByIdAndDelete(req.params.id);
+            await autores.findByIdAndDelete(req.params.id);
             res.status(200).json({ message: "Autor removido com sucesso."})
 
         } catch (error) {;
-            res.status(500).json({message: `Falha na exclusão do autor: ${error.message}`});
+            res.status(500).json({ message: `Falha na exclusão do autor: ${error.message}` });
         }
     };
 };

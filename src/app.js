f40f41ef85e734aadeff1328240424b6daf55670
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-20 22:27:23 -0300

    :recycle: refactor: Adicionado uso de middleware handler404 para tratrativa de rotas não localizadas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import handler404 from "./middlewares/handler404.js";
 
 db.on("error",  console.log.bind("Erro durante a conexão com o banco de dados"));
 db.once("open", () => {
@@ -11,6 +12,8 @@ db.once("open", () => {
 const app = express();
 app.use(express.json());
 routes(app);
+
+app.use(handler404);
 
 app.use(errorHandler);
 

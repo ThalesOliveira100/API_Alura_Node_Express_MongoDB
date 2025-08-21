cd40a4e67fd557cbf7ff9a23d319d792af0a3478
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-20 21:24:00 -0300

    :recycle: refactor: Adicionado validação de erros do tipo ValidationError

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
 // eslint-disable-next-line no-unused-vars
-function errorHandler(erro, req, res, next) {
-    console.log(erro);
+function errorHandler(err, req, res, next) {
+    console.log(err);
 
-    if (erro instanceof mongoose.Error.CastError) {
+    if (err instanceof mongoose.Error.CastError) {
         res.status(400).json({message: "Um ou mais dados fornecidos estão incorretos."});
+    } else if(err instanceof mongoose.Error.ValidationError) {
+        const mensagensErro = Object.values(err.errors)
+            .map(err => err.message)
+            .join("; ");
+
+        res.status(400).json({message: `Os seguintes erros foram encontrados: ${mensagensErro}`});
     } else {
         res.status(500).json({ message: "Erro  interno do servidor."});
     };

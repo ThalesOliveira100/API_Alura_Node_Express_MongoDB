e5cf86432dfd17d8d0da433d4bd604f0b167c7d0
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-20 22:28:18 -0300

    :recycle: refactor: Adicionado tratamento para situações de NotFound Error

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import NotFound from "../errs/NotFound.js";
 import livros from "../models/Livro.js";
 
 class LivroController {
@@ -8,8 +9,11 @@ class LivroController {
                 .populate("autor")
                 .exec();
 
-            res.status(200).json(listaLivros);
-            
+            if (listaLivros.length !== 0) {
+                res.status(200).json(listaLivros);
+            } else {
+                next(new NotFound("Nenhum livro foi encontrado."));
+            };            
         } catch (err) {
             next(err);
         };
@@ -20,9 +24,12 @@ class LivroController {
             const livroEncontrado = await livros.findById(req.params.id)
                 .populate("autor", "nome")
                 .exec();
-
-            res.status(200).json(livroEncontrado);
-
+            
+            if (livroEncontrado !== null) {
+                res.status(200).json(livroEncontrado);
+            } else {
+                next(new NotFound("Id do Livro não encontrado."));
+            };
         } catch (err) {
             next(err);
         };
@@ -31,8 +38,7 @@ class LivroController {
     static async cadastrarLivro(req, res, next) {
         try {
             let livro = new livros(req.body);
-
-            const livroResultado = await livro.save()
+            const livroResultado = await livro.save();
 
             res.status(201).send(livroResultado.toJSON());
             
@@ -44,10 +50,12 @@ class LivroController {
     static async atualizarLivro (req, res, next) {
         try {
             const id = req.params.id;
-            await livros.findByIdAndUpdate(id, {$set: req.body});
-
-            res.status(200).json({ message: "Livro atualizado"});
-
+            
+            if (await livros.findByIdAndUpdate(id, {$set: req.body}) !== null) {
+                res.status(200).json({ message: "Livro atualizado"});
+            } else {
+                next(new NotFound("Id do Livro não encontrado."));
+            };
         } catch (err) {
             next(err);
         };
@@ -55,9 +63,11 @@ class LivroController {
 
     static async excluirLivro (req, res, next) {
         try {
-            await livros.findByIdAndDelete(req.params.id);
-            res.status(200).json({ message: "Livro removido com sucesso."})
-
+            if (await livros.findByIdAndDelete(req.params.id) !== null) {
+                res.status(200).json({ message: "Livro removido com sucesso."})
+            } else {
+                next(new NotFound("Id do Livro não encontrado."));
+            };
         } catch (err) {
             next(err);
         };
@@ -66,10 +76,13 @@ class LivroController {
     static async listarLivrosPorEditora (req, res, next) {
         try {
             const editora = req.query.editora;
-
             const livrosPorEditora = await livros.find({"editora": editora });
-            res.status(200).json(livrosPorEditora);
-            
+
+            if (livrosPorEditora.length !== 0) {
+                res.status(200).json(livrosPorEditora);
+            } else {
+                next(new NotFound("Nenhum livro atribuído à editora foi encontrado."));
+            };
         } catch (err) {
             next(err);
         };

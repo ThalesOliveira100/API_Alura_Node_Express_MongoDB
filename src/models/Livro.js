b012c7a0720e25842b49c825dcac4eb97c4a9da5
Thales Oliveira <TOliveiraDev@outlook.com>
2025-08-27 15:15:49 -0300

    :recycle: refactor: Alterado validação de numero de páginas mínima e máxima para usar a custom validator

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -23,8 +23,12 @@ const livrosSchema = new mongoose.Schema(
         preco: { type: Number },
         paginas: { 
             type: Number,
-            min: [10, "O número de páginas deve estar entre 10 e 5000. Valor Fornecido: {VALUE}"],
-            max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor Fornecido: {VALUE}"]
+            validate: {
+                validator: (valor) => {
+                    return valor >= 10 && valor <= 5000;
+                },
+                message: "O número de páginas deve estar entre 10 e 5000. Valor Fornecido: {VALUE}"
+            }
         }
     }, 
     { 
